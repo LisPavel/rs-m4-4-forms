800b36e89402b68ad89324d1b4a68341aacf9d59
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2024-02-17 14:56:11 +0300

    feat: add signup form

--- src/components/Signup/index.tsx
+++ src/components/Signup/index.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { ChangeEvent, FormEventHandler, useRef } from "react";
+import TextInput from "../TextInput";
+
+import styles from "./index.module.scss";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const formStateRef = useRef({
+    name: "",
+    nickname: "",
+    email: "",
+    gender: "",
+    password: "",
+    passwordRepeat: "",
+  });
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (ev): void => {
+    ev.preventDefault();
+    console.log(formStateRef.current);
+  };
+  const handleChange: FormEventHandler = (
+    ev: ChangeEvent<HTMLInputElement>,
+  ): void => {
+    formStateRef.current = {
+      ...formStateRef.current,
+      [ev.target.name]: ev.target.value,
+    };
+  };
+  return (
+    <form
+      className={styles.signup}
+      onChange={handleChange}
+      onSubmit={handleSubmit}
+    >
+      <h3>Signup</h3>
+      <TextInput name="name" label="Имя" />
+      <TextInput name="nickname" label="Ник" />
+      <TextInput name="email" label="Почта" />
+      <TextInput name="gender" label="Пол" />
+      <TextInput name="password" type="password" label="Пароль" />
+      <TextInput
+        name="passwordRepeat"
+        type="password"
+        label="Повторить пароль"
+      />
+      <button type="submit">Зарегистрироваться</button>
+    </form>
+  );
 };
 
 export default Signup;

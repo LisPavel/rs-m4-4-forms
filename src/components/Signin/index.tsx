ccaf2ad31d4971666d191cfd610f510593b90a95
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2024-02-17 14:40:40 +0300

    feat: add signin form

--- src/components/Signin/index.tsx
+++ src/components/Signin/index.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { ChangeEvent, FormEventHandler, useRef } from "react";
+import TextInput from "../TextInput";
+import styles from "./index.module.scss";
 
 const Signin = () => {
-  return <div>Signin</div>;
+  const formDataRef = useRef({ email: "", password: "" });
+
+  const handleFormChange: FormEventHandler = (
+    ev: ChangeEvent<HTMLInputElement>,
+  ): void => {
+    formDataRef.current = {
+      ...formDataRef.current,
+      [ev.target.name]: ev.target.value,
+    };
+  };
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (ev) => {
+    ev.preventDefault();
+    console.log(formDataRef.current);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      onChange={handleFormChange}
+      className={styles.singin}
+    >
+      <h3>Signin form</h3>
+      <TextInput name="email" label="Email" />
+      <TextInput name="password" type="password" label="Password" />
+      <button type="submit">Войти</button>
+    </form>
+  );
 };
 
 export default Signin;
